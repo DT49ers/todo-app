2ece198dd1b5c626ff17ed2d1ff29470bdd19822
JOSE TOVAR <jdtovar878@JOSEs-MacBook-Pro.local>
2024-09-22 19:15:59 -0400

    first react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
+// Functional component
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+  const addTodos = (e) => {
+    e.preventDefault(); // This prevents a REFRESH which is the default behavior after form submit from button
+    setTodos([...todos, input]);
+    setInput("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <center>
+        <h1>Welcome to My TODO List</h1>
+        <form>
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button style={{ color: "red" }} type="submit" onClick={addTodos}>
+            ADD TODO
+          </button>
+        </form>
+        <h2>List of Todos</h2>
+        {todos.map((todo, i) => (
+          <p>
+            {i + 1}. {todo}
+          </p>
+        ))}
+      </center>
     </div>
   );
 }
